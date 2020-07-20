47ec88894ca626d85ecd07ac682c915e01a403b4
Davis Lindell <daviselindell@gmail.com>
2020-07-20 12:24:09 -0500

    refactored classes for inheirtance

--- index.py
+++ index.py
@@ -1,21 +1,29 @@
 from attractions import *
 from animals import *
 
-malcom = Frog('Malcom', 'Tree', 'crickets')
-miles = Toad('Miles', 'Sonoran Desert', 'worms')
-zane = Spider('Zane', 'Black Widow', 'morning', 'humans')
+malcom = Frog('Malcom', 'Tree', 'crickets', 123454)
+miles = Toad('Miles', 'Sonoran Desert', 'worms', 45678)
+zane = Spider('Zane', 'Black Widow', 'humans', 4564)
 adrian = Lizard('Adrian', 'Chameleon', 'midday', 'maggots')
-tom = Snake('Tom', 'Copperhead', 'mice')
-leigha = Goldfish('Leigha', 'Gold Goldfish', 'fish food')
-bryan = Loon('Bryan', 'Northern', 'cherrios')
-joe = Goose('Joe', 'Canadian', 'minos')
-sarah = Flamingo('Sarah', 'Purple', 'mosquitos')
-luke = Duck('Luke', 'Mallard', 'bread')
-eli = Groundhog('Eli', 'Nusance', 'afternoon', 'trash')
-kaleb = Squirrel('Kaleb', 'Grey', 'morning', 'nuts')
-michelle = Cat('Michelle', 'Mountain Lion', 'afternoon', 'sardines')
-brian = Dog('Brian', 'Corgi', 'midday', 'wal-mart meat truck leftovers')
-kirk = Horse('Kirk', 'Thoroughbred', 'morning', 'grains')
+tom = Snake('Tom', 'Copperhead', 'mice', 4589897)
+leigha = Goldfish('Leigha', 'Gold Goldfish', 'fish food', 21534)
+bryan = Loon('Bryan', 'Northern', 'cherrios', 154684)
+joe = Goose('Joe', 'Canadian', 'minos', 46848)
+sarah = Flamingo('Sarah', 'Purple', 'mosquitos', 4846848)
+luke = Duck('Luke', 'Mallard', 'bread', 48949)
+eli = Groundhog('Eli', 'Nusance', 'afternoon', 'trash', 51684)
+kaleb = Squirrel('Kaleb', 'Grey', 'morning', 'nuts', 156814894)
+michelle = Cat('Michelle', 'Mountain Lion', 'afternoon', 'sardines', 18181)
+brian = Dog('Brian', 'Corgi', 'midday', 'wal-mart meat truck leftovers', 1846)
+kirk = Horse('Kirk', 'Thoroughbred', 'morning', 'grains', 12345)
+
+
+print(kirk.chip_number)
+
+kirk.chip_number = 57889
+
+print(kirk.chip_number)
+
 
 varmint_village = PettingZoo('Varmint Village')
 varmint_village.animals.extend([kirk, brian, michelle, kaleb, eli])
@@ -38,3 +46,5 @@ print(dirty_pond)
 for animal in dirty_pond.animals:
     print(f'{animal.name} the {animal.species}')
 
+
+print(dirty_pond.last_critter_added)
